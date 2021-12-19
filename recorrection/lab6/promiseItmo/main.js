57cab452881f8bb9fc62912be6a0687ed12ea9e5
who !!! <62444527+kol9999@users.noreply.github.com>
2021-12-20 01:19:10 +0300

    Add files via upload

---
+++ recorrection/lab6/promiseItmo/main.js
@@ -0,0 +1,24 @@
+
+window.onload = function(){        
+const preloader = document.querySelector(".preloader");
+    preloader.className += " hidden";       
+  fetch(`https://jsonplaceholder.typicode.com/comments?_start=0&_limit=${Math.floor(Math.random() * 40)}`)
+       .then((res)=>res.json())
+       .then((data)=>{
+       let output = '<template></template>';
+       data.forEach(function(todo){
+        output += `
+<div class="card" style="width: 14rem;">
+  <img class="card-img-top" src="https://marmotamaps.com/de/fx/wallpaper/download/faszinationen/Marmotamaps_Wallpaper_Berchtesgaden_Desktop_1920x1080.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">${todo.email}</p
+    <p class="card-text">${todo.body}</p>
+  </div>
+</div>
+      `;
+      console.log(data)
+    });
+    document.getElementById('posts').innerHTML=output;
+});   
+}
+//The variable output is a template tag, also removed all bootstrap related classes, added borders and sizes with pure css and the page is somewhat mobile :)
