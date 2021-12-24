d4bce0cb4ea54e51d0e62a2a4a862a78bb6403d6
who !!! <62444527+kol9999@users.noreply.github.com>
2021-12-24 09:09:26 +0300

    Delete lab4_again directory

--- lab4_again/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function($) {
- let url = document.location.href;
-  $('nav ul li a').each(function() {
-   if (this.href === url) {
-   $(this).addClass('active');
-  }
- });
-  console.log(url)
-});
